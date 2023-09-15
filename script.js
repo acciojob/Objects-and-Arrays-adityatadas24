a61441baa3453e981c6e7c94a6bd463dd5d5a79f
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-15 15:47:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const team = players;
+
+// Create a shallow copy of the array 'players' using the spread operator
+const team1 = [...players];
+
+// Create a shallow copy of the 'person' object using the spread operator
+const cap1 = { ...person };
+
+// Test if the variables are working as expected
+console.log("team:", team);
+console.log("team1:", team1);
+console.log("cap1:", cap1);
 
 window.players = players;
 window.person = person;
